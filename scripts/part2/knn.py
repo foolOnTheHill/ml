93e3f26cd1eafe3c37d35a5ee08cda643fdfe144
George H. Alvares <ghao@cin.ufpe.br>
2015-10-06 13:19:31 -0300

    speeding up distance computing

--- scripts/part2/knn.py
+++ scripts/part2/knn.py
@@ -4,7 +4,6 @@ def estimatePosterior(X, Y, W, k):
     n = len(X)
     c = len(W)
     ids = range(n)
-    indexed = zip(ids, X)
 
     delta = lambda (x_ik, x_jk) : 0 if (x_ik == x_jk) else 1
     d = lambda x_i, x_j : sum(map(delta, zip(x_i, x_j))) # Dissimmilarity function
@@ -12,11 +11,16 @@ def estimatePosterior(X, Y, W, k):
     for j in range(c):
         P.append([])
 
+    ND = [[0 for j in range(n)] for i in range(n)] # Matrix filled with 0s
+
     for i in range(n):
-        # TODO: since d(-, -) is symmetrical, we can speed up and avoid re-doing some calculations to compute N.
-        mp = lambda (k, x) : (k, d(X[i], x))
-        ft = lambda (k, x) : not (k == i)
-        N = map(mp, filter(ft, indexed)) # Neighbors
+        for j in range(i+1, n):
+            ND[i][j] = d(X[i], X[j])
+            ND[j][i] = ND[i][j]
+
+    for i in range(n):
+        ft = lambda (k, x) : not (k == i) # Drops the element with index eq. to i
+        N = filter(ft, zip(ids, ND[i])) # Neighbors
         N.sort(lambda (l, a), (m, b) : -1 if (a < b) else 1 if (a > b) else 0)
         N = N[:k] # K Nearest Neighbors
 
