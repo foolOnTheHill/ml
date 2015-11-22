7ffdd4f9d8d19b6ed442cbce6c3cb088e87a28e6
George Oliveira <ghao@cin.ufpe.br>
2015-11-22 20:53:29 -0300

    fixing a lot of stuff
    momentum, mse measure, etc

--- scripts/part2/mlp.py
+++ scripts/part2/mlp.py
@@ -1,14 +1,31 @@
 # Author: George Oliveira (ghao@cin.ufpe.br) - 2015
 #
-# Vaguely based on: https://web.stanford.edu/class/cs294a/sparseAutoencoder.pdf
+# References:
+# - https://web.stanford.edu/class/cs294a/sparseAutoencoder.pdf
+# - http://www.ra.cs.uni-tuebingen.de/SNNS/UserManual/node146.html
+
+# Dataset info:
 #
-# TODO:
-# - Create a MLP class;
-# - Create accuracy measures;
+# Total: 958 data points
+# 2 classes (positive, negative)
+#
+# - positive: 626
+# - negative: 332
+#
+# Experiments division:
+#
+# - positive:
+#     => Train: 313
+#     => Val: 157
+#     => Test: 155
+#
+# - negative:
+#     => Train: 166
+#     => Val: 83
+#     => Test: 83
 
 import random
-# from sklearn import datasets, metrics
-from math import exp
+from math import tanh
 
 random.seed(42)
 
@@ -16,22 +33,58 @@ def sigmoid(x):
     """ Sigmoid function. """
     return 1.0 / (1.0 + exp(-x))
 
-def deriv(x):
-    """ Derivative of the Sigmoid function. """
-    return sigmoid(x)*(1 - sigmoid(x))
+def mse(h, y):
+    """
+        Mean Squared Error.
 
-def train(X, Y, learning_rate, hidden_units, output_units, max_iterations):
+        Parameters:
+            - h : the output given by the network;
+            - y : the expected output.
+    """
+    d = sum([(h[i] - y[i])**2 for i in range(len(y))])
+    return 0.5*d
+
+def netMSE(X, Y, W, b, hidden_units, output_units):
+    """
+        Computes the MSE for the current network to perform the validation test run while training.
+
+        Parameters:
+            - X : validation data inputs;
+            - Y : validation data outputs;
+            - W : weights matrix of the network;
+            - b : biases matrix of the network;
+            - hidden_units : a list containing the number of units per hidden layer;
+            - output_units : number of output units (must be equal to the dimension of the elements on Y).
+    """
+
+    dim = len(X[0]) # inputs dimension
+
+    layers = 1 + len(hidden_units) + 1 # input layer + hidden layer + output layer
+    units = [dim] + hidden_units + [output_units]
+
+    m = len(X)
+    P = fit(X, W, b, hidden_units, output_units)
+
+    sm = sum([mse(Y[i], P[i]) for i in range(m)]) / float(m)
+
+    return sm
+
+def train(X, Y, testX, testY, learning_rate, hidden_units, output_units, max_iterations=200, threshold=0.05):
     """
         Uses Backpropagation Gradient Descent to train a Multi-layer Perceptron.
 
         Parameters:
-            - X : Train data inputs;
-            - Y : Train data outputs;
+            - X : train data inputs;
+            - Y : train data outputs;
+            - testX : validation inputs;
+            - testY : validation outputs;
             - learning_rate : MLP learning rate;
             - hidden_units : a list containing the number of units per hidden layer;
             - output_units : number of output units (must be equal to the dimension of the elements on Y);
-            - max_iterations : maximum number of iterations of the training algorithm.
+            - max_iterations : maximum number of iterations of the training algorithm;
+            - threshold : MSE limit.
     """
+
     dim = len(X[0]) # inputs dimension
 
     layers = 1 + len(hidden_units) + 1 # input layer + hidden layer + output layer
@@ -40,16 +93,21 @@ def train(X, Y, learning_rate, hidden_units, output_units, max_iterations):
     W = [] # Weights matrix
     b = [] # Bias matrix
 
+    M = [] # Momentum
+    alpha = 0.001 # Momentum influence
+
     # Initializes W and b with random values
     for l in range( layers-1 ): # except for output
-        W.append([])
-        b.append( random.uniform(0, 1) )
+        W.append( [] )
+        M.append( [] )
+        b.append( [] )
         for i in range( units[l] ) :
             W[l].append([])
+            M[l].append([])
             for j in range( units[l+1] ) :
-                W[l][i].append( random.uniform(0, 1) ) # sets a random weight W[l][i][j] from unit i in layer l to unit j in layer l+1
-
-    b.append( random.uniform(0, 1) ) # bias for output layer
+                b[l].append( random.uniform(0, 0.2) ) # sets random biases
+                W[l][i].append( random.uniform(0, 0.2) ) # sets a random weight W[l][i][j] from unit i in layer l to unit j in layer l+1
+                M[l][i].append( 0 ) # the momentum term has no influence on the first iteration
 
     for it in range(max_iterations):
         for k in range(len(X)):
@@ -63,84 +121,150 @@ def train(X, Y, learning_rate, hidden_units, output_units, max_iterations):
                 a.append([])
                 z.append([])
                 for j in range(units[l]):
-                    sm = -b[l]
+                    sm = -b[l-1][j]
                     for i in range(units[l-1]):
                         sm += a[l-1][i]*W[l-1][i][j]
-                    a[l].append( sigmoid(sm) )
+                    out = sm if l == layers-1 else tanh(sm) # the output layer is linear
+                    a[l].append( out )
                     z[l].append( sm )
 
             # Backward step
             delta = [[0 for j in range(units[i])] for i in range(layers)]
 
             for i in range(output_units): # Computes delta only for the output layer
-                delta[layers-1][i] = - ( opt[i] - a[layers-1][i] ) * deriv( z[layers-1][i] )
+                delta[layers-1][i] = - ( opt[i] - a[layers-1][i] ) * (1 - a[layers-1][i]*a[layers-1][i])
 
-            for l in range(layers-2, 0, -1): # Backpropagates the error
+            for l in range(layers-2, -1, -1): # Backpropagates the error
                 for i in range( units[l] ):
                     sm = 0
                     for j in range( units[l+1] ) :
-                        sm += W[l][i][j]*delta[l+1][j]
-                    delta[l][i] = sm * deriv( z[l][i] )
+                            sm += W[l][i][j]*delta[l+1][j]
+                    delta[l][i] = sm * a[l][i] * (1 - a[l][i])
 
             for l in range(layers-1):
                 for i in range(units[l]):
                     for j in range(units[l+1]):
                         # Gradient Descent
-                        W[l][i][j] -= learning_rate * a[l][i] * delta[l+1][j]
-                        b[l] -= learning_rate * delta[l+1][j]
+                        W[l][i][j] += learning_rate * a[l][i] * delta[l+1][j] + alpha*M[l][i][j]
+                        M[l][i][j] = learning_rate * a[l][i] * delta[l+1][j]
+                        b[l][j] += learning_rate * delta[l+1][j]
+
+        error = netMSE(testX, testY, W, b, hidden_units, output_units)
+
+        if it%100 == 0:
+            print "Step #%d" % (it+1)
+            print "Error %f" % (error*100)
+
+        if error < threshold:
+            break
 
     return (W, b)
 
-def classify(X, W, b, hidden_units, output_units):
+def fit(X, W, b, hidden_units, output_units):
     """
         Given the Weights matrix and the bias representing a Multi-layer Perceptron, classifies the data present on X.
 
         Parameters:
             - X : the data that will be classified;
-            - W : the weights matrix;
-            - b : the bias for each layer;
+            - W : the weights matrix of the network;
+            - b : the biases matrix of the network;
             - hidden_units: a list containing the number of units per hidden layer;
             - output_units : number of output units on the Multi-layer Perceptron.
     """
+
     dim = len(X[0])
     layers = 1 + len(hidden_units) + 1
     units = [dim] + hidden_units + [output_units]
 
+    o = []
     for k in range(len(X)):
         ipt = X[k]
         a = [ipt]
         for l in range(1, layers):
             a.append([])
             for j in range(units[l]):
-                sm = -b[l]
+                sm = -b[l-1][j]
                 for i in range(units[l-1]):
                     sm += a[l-1][i]*W[l-1][i][j]
-                a[l].append( sigmoid(sm) )
+                out = sm if l == layers-1 else tanh( sm )
+                a[l].append( out )
+        o.append(a[layers-1])
 
-    return a[layers-1]
+    pred = []
+    for i in range(len(X)):
+        c = 0
+        p = o[i][0]
+        for j in range(1, output_units):
+            if o[i][j] > p:
+                c = j
+                p = o[i][j]
+        class_bits = [0 for k in range(output_units)]
+        class_bits[c] = 1
+        pred.append(class_bits)
 
-# # Uncomment if you have 'sklearn'
-# if __name__ == "__main__":
-#     iris = datasets.load_iris()
-#
-#     to_list = lambda x : [x]
-#
-#     iris.target = map(to_list, iris.target)
-#
-#     trainX = iris.data[:135]
-#     trainY = iris.target[:135]
-#
-#     testX = iris.data[136:]
-#     testY = iris.target[136:]
-#
-#     (W, b) = train(trainX, trainY, 0.1, [10, 20, 10], 1, 200)
-#
-#     pred = classify(testX, W, b, [10, 20, 10], 1)
-#
-#     error = 0
-#     for i in range(len(pred)):
-#         if pred[i] != testY[i]:
-#             error += 1
-#
-#     score =  error / float(len(testX))
-#     print "Accuracy %f" % score
+    return pred
+
+def normalize(X, Y, num_classes=0):
+    """
+        Pre-processes the data that will be used to train the network.
+    """
+
+    for i in range(len(X)):
+        c = [0 for k in range(num_classes)]
+        c[Y[i][0]] = 1
+        Y[i] = c
+
+    return (X, Y)
+
+def getClass(class_bits):
+    for c in range(len(class_bits)):
+        if class_bits[c] == 1:
+            return c
+
+def readData(filename):
+    """ Reads a dataset from `filename` and returns it as a matrix. """
+    f = open(filename, 'r')
+    lines = f.readlines()
+    # Pre-proccesses the data by removing the semicolons
+    mp = lambda l : l.split(',')
+    data = map(mp, lines)
+    getX = lambda l : l[:len(l)-1]
+    X = map(getX, data)
+    getY = lambda l : l[-1][:-1] # Gets the last element (the class parameter)
+    Y = map(getY, data)
+    return (X, Y)
+
+def proccessData(filename):
+    """ Maps the input data to the model defined at the documentation. """
+    (X, Y) = readData(filename)
+    mp = lambda x : 1 if x == 'x' else 0 if x == 'o' else -1
+    proccessX = lambda l : map(mp, l)
+    X = map(proccessX, X)
+    proccessY = lambda y : [1] if y == 'positive' else [0]
+    Y = map(proccessY, Y)
+    return normalize(X, Y, num_classes=2)
+
+def loadData():
+    """ Reads and pre-processes the dataset. """
+    return proccessData('tic-tac-toe.data.txt')
+
+if __name__ == "__main__":
+    # (dataX, dataY) = loadData()
+    #
+    # trainX = dataX[:500]
+    # trainY = dataY[:500]
+    #
+    # testX = dataX[501:]
+    # testY = dataY[501:]
+    #
+    # hidden_units = [4, 15, 4]
+    # learning_rate = 0.1
+    #
+    # (W, b) = train(trainX, trainY, testX, testY, learning_rate, hidden_units, 2, 500)
+
+    # P = fit(trainX, W, b, hidden_units, 2, False)
+    #
+    # for i in range(len(P)):
+    #     print "Input: "+str(testX[i])
+    #     print "Predict "+str(P[i])
+    #     print "Expected "+str(testY[i])
