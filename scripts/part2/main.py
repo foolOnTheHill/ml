c9aeb4e260b088bef9f213449e92599aa9e9f987
George Oliveira <ghao@cin.ufpe.br>
2015-12-02 20:58:55 -0300

    fixing file parameter to append

--- scripts/part2/main.py
+++ scripts/part2/main.py
@@ -30,16 +30,16 @@ def writeResults(f, e_rate, se, interval):
 def run():
     classificadores = ["Bayesian", "KNN 1", "KNN 5", "KNN 10", "KNN 20", "KNN 30", "Sum 1", "Sum 5", "Sum 10", "Sum 20", "Sum 30"]
     errorResults = {}
-    
+
     for c in classificadores:
         errorResults[c] = []
-        
+
     K = [30, 20, 10, 5, 1]
 
-    resultsBay = open('part2-results-bayesian.txt', 'w')
-    resultsKn = open('part2-results-knn.txt', 'w')
-    resultsSum = open('part2-results-sum.txt', 'w')
-    
+    resultsBay = open('part2-results-bayesian.txt', 'a')
+    resultsKn = open('part2-results-knn.txt', 'a')
+    resultsSum = open('part2-results-sum.txt', 'a')
+
     for j in range(10):
         H = data_proccessing.loadData() # Folds
 
@@ -47,14 +47,14 @@ def run():
             resultsBay.write("Round %d\n\n" % (j*10+i+1))
             resultsKn.write("Round %d\n\n" % (j*10+i+1))
             resultsSum.write("Round %d\n\n" % (j*10+i+1))
-    
+
             (trainX, trainY, testX, testY) = prepareSets(H, i)
             (P_bay, E_bay, e_rate_bay, se_bay, interval_bay) = bayesian.classify(trainX, trainY, testX, testY)
-    
+
             resultsBay.write("- Bayesian\n")
             writeResults(resultsBay, e_rate_bay, se_bay, interval_bay)
             errorResults["Bayesian"].append(e_rate_bay)
-    
+
             for k in K:
                 (P_kn, E_kn, e_rate_kn, se_kn, interval_kn) = knn.classify(trainX, trainY, testX, testY, k)
                 (P_sum, E_sum, e_rate_sum, se_sum, interval_sum) = sum_rule.classify([P_bay, P_kn], testX, testY)
@@ -70,7 +70,7 @@ def run():
     resultsBay.close()
     resultsKn.close()
     resultsSum.close()
-    
+
     compare(errorResults)
 
 if __name__ == '__main__':
