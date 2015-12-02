acd1a33a30b9956accaf65850e7b30047de8c0e3
lns2 <lns2@cin.ufpe.br>
2015-12-02 21:16:57 -0200

    Repetindo experimento 10 vezes

--- scripts/part2/main.py
+++ scripts/part2/main.py
@@ -34,36 +34,38 @@ def run():
     for c in classificadores:
         errorResults[c] = []
         
-    H = data_proccessing.loadData() # Folds
     K = [30, 20, 10, 5, 1]
 
     resultsBay = open('part2-results-bayesian.txt', 'w')
     resultsKn = open('part2-results-knn.txt', 'w')
     resultsSum = open('part2-results-sum.txt', 'w')
+    
+    for j in range(10):
+        H = data_proccessing.loadData() # Folds
 
-    for i in range(10):
-        resultsBay.write("Round %d\n\n" % (i+1))
-        resultsKn.write("Round %d\n\n" % (i+1))
-        resultsSum.write("Round %d\n\n" % (i+1))
-
-        (trainX, trainY, testX, testY) = prepareSets(H, i)
-        (P_bay, E_bay, e_rate_bay, se_bay, interval_bay) = bayesian.classify(trainX, trainY, testX, testY)
-
-        resultsBay.write("- Bayesian\n")
-        writeResults(resultsBay, e_rate_bay, se_bay, interval_bay)
-        errorResults["Bayesian"].append(e_rate_bay)
-
-        for k in K:
-            (P_kn, E_kn, e_rate_kn, se_kn, interval_kn) = knn.classify(trainX, trainY, testX, testY, k)
-            (P_sum, E_sum, e_rate_sum, se_sum, interval_sum) = sum_rule.classify([P_bay, P_kn], testX, testY)
-            #
-            resultsKn.write("- KNN (n = %d)\n" % k)
-            writeResults(resultsKn, e_rate_kn, se_kn, interval_kn)
-            errorResults["KNN %i" % k].append(e_rate_kn)
-            #
-            resultsSum.write("- Sum (n = %d)\n" % k)
-            writeResults(resultsSum, e_rate_sum, se_sum, interval_sum)
-            errorResults["Sum %i" % k].append(e_rate_sum)
+        for i in range(10):
+            resultsBay.write("Round %d\n\n" % (i+1))
+            resultsKn.write("Round %d\n\n" % (i+1))
+            resultsSum.write("Round %d\n\n" % (i+1))
+    
+            (trainX, trainY, testX, testY) = prepareSets(H, i)
+            (P_bay, E_bay, e_rate_bay, se_bay, interval_bay) = bayesian.classify(trainX, trainY, testX, testY)
+    
+            resultsBay.write("- Bayesian\n")
+            writeResults(resultsBay, e_rate_bay, se_bay, interval_bay)
+            errorResults["Bayesian"].append(e_rate_bay)
+    
+            for k in K:
+                (P_kn, E_kn, e_rate_kn, se_kn, interval_kn) = knn.classify(trainX, trainY, testX, testY, k)
+                (P_sum, E_sum, e_rate_sum, se_sum, interval_sum) = sum_rule.classify([P_bay, P_kn], testX, testY)
+                #
+                resultsKn.write("- KNN (n = %d)\n" % k)
+                writeResults(resultsKn, e_rate_kn, se_kn, interval_kn)
+                errorResults["KNN %i" % k].append(e_rate_kn)
+                #
+                resultsSum.write("- Sum (n = %d)\n" % k)
+                writeResults(resultsSum, e_rate_sum, se_sum, interval_sum)
+                errorResults["Sum %i" % k].append(e_rate_sum)
 
     resultsBay.close()
     resultsKn.close()
